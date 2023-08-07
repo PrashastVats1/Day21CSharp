af9dddfd0f634f3b437111b472a134a2a3afe6c5
PrashastVats1 <prashastvats@gmail.com>
2023-08-07 15:06:15 +0530

    Assignment Binary XML Serializing and Deserializing

---
+++ ConAppBinaryXMLSerDeSer/ConAppBinaryXMLSerDeSer/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using EmployeeLib;
+
+namespace ConAppBinaryXMLSerDeSer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"D:\Mphasis\Live Session\Day21\Assignment 24";
+            string binaryFilePath = Path.Combine(folderPath, "employee.bin");
+            string xmlFilePath = Path.Combine(folderPath, "employee.xml");
+            Employee employee = new Employee();
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Employee));
+
+            //Binary Serialization
+            using(FileStream fs = new FileStream(binaryFilePath, FileMode.Create))
+            {
+                //Some sample employee data
+                employee.Id = 4;
+                employee.FirstName = "Sung";
+                employee.LastName = "Jinwoo";
+                employee.Salary = 80000000.75;
+
+                binaryFormatter.Serialize(fs, employee);
+                Console.WriteLine("Employee data has been serialized to Binary format");
+            }
+            //Binary Deserialization
+            using(FileStream fs = new FileStream(binaryFilePath, FileMode.Open))
+            {
+                Employee deserializedEmployee = (Employee)binaryFormatter.Deserialize(fs);
+                Console.WriteLine("\nDesrialized Employee Data (Binary File)");
+                Console.WriteLine($"ID: \t{deserializedEmployee.Id}");
+                Console.WriteLine($"First Name: \t{deserializedEmployee.FirstName}");
+                Console.WriteLine($"Last Name: \t{deserializedEmployee.LastName}");
+                Console.WriteLine($"Salary: \t{deserializedEmployee.Salary}");
+            }
+
+            //XML Serialization
+            using(TextWriter writer = new StreamWriter(xmlFilePath))
+            {
+                xmlSerializer.Serialize(writer, employee);
+                Console.WriteLine("\nEmployee data has been serialized to XML format");
+            }
+            //XML Deserialization
+            using(TextReader reader = new StreamReader(xmlFilePath))
+            {
+                Employee deserializedEmployee = (Employee)xmlSerializer.Deserialize(reader);
+                Console.WriteLine("\nDeserialized Employee Data (XML)");
+                Console.WriteLine($"ID: \t{deserializedEmployee.Id}");
+                Console.WriteLine($"First Name: \t{deserializedEmployee.FirstName}");
+                Console.WriteLine($"Last Name: \t{deserializedEmployee.LastName}");
+                Console.WriteLine($"Salary: \t{deserializedEmployee.Salary}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConAppBinaryXMLSerDeSer/ConAppBinaryXMLSerDeSer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConAppBinaryXMLSerDeSer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("ConAppBinaryXMLSerDeSer")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("78d0f549-e0af-462e-8cee-931bb303180c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
